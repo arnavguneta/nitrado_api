627b12d49abfd025c10560e104b237658e24a3a6
Arnav Guneta <arnavguneta@gmail.com>
2022-06-16 18:07:58 -0400

    init setup for strmr log/discord integration

--- src/index.js
+++ src/index.js
@@ -9,13 +9,51 @@ const config = require('@root/config.json');
 
 const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES, Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS], partials: Object.values(Discord.Constants.PartialTypes) })
 
+let webhook;
+
 client.on('ready', async () => {
     if (config.settings.maintenance) {
         config.settings.maintenance = false
         fs.writeFile('./src/config.json', JSON.stringify(config), err => { })
     }
+
+    // webhook fetcher
+    const channel = client.channels.cache.get('987101924266496000');
+
+	try {
+		const webhooks = await channel.fetchWebhooks();
+		webhook = webhooks.find(wh => wh.token);
+
+		if (!webhook) {
+			return console.log('No webhook was found that I can use!');
+		}
+	} catch (error) {
+		console.error('Error trying to send a message: ', error);
+	}
+
+    // start listener for streamer logs
+    let last_message = require('@root/last-message.json');
+    setInterval(async () => {
+        let post_fetch = await fetch(`https://logs.ivr.fi/channel/erobb221/user/erobb221?json=true&reverse=true`);
+        let data = await post_fetch.json()
+
+        let new_messages = await data.messages.filter(message => message.tags['tmi-sent-ts'] > last_message.tags['tmi-sent-ts'])
+        for (let i = new_messages.length-1; i >= 0; i--) sendMessage(new_messages[i].text, new_messages[i].username, 'https://cdn.betterttv.net/emote/5fc53fdecac2fb4621e48bb0/3x')
+        
+        last_message = data.messages[0]
+        fs.writeFile('./src/last-message.json', JSON.stringify(last_message), err => { })
+    }, 10000)
+
     console.log("Bot is ready!")
 })
+
+const sendMessage = async (content, streamer, avatar) => {
+    await webhook.send({
+        content: content,
+        username: streamer,
+        avatarURL: avatar,
+    });
+}
 
 client.on('messageCreate', async message => {
     let { content } = message
